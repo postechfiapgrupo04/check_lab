c9bd99c8bd24b87ed3a88ee9d33823bdc9751f9f
brunorosselli <bruno.rosselli@hotmail.com>
2024-01-17 18:15:52 -0300

    [FIAP-G04] Create Flow Check Project

---
+++ src/main/java/br/com/fiap/flow/check/flow/check/FlowCheckApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.flow.check.flow.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/flow/check/flow/check/FlowCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.flow.check.flow.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
