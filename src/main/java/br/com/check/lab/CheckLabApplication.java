ddae3beba6f3e1155232f924f60f1c25faae26f4
brunorosselli <bruno.rosselli@hotmail.com>
2024-01-19 19:35:22 -0300

    [GRUPO-04] Create Flow Lab Project

---
+++ src/main/java/br/com/check/lab/CheckLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.check.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckLabApplication.class, args);
+	}
+
+}
